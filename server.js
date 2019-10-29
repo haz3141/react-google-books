4c0493267db5535f5629f4be72073775439ce8d3
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-10-29 19:53:51 -0400

    Mongo DB connected

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+
+// Set Mongoose DB URI
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://haz3141:Pw3141!@ds241278.mlab.com:41278/heroku_pjvjkvk7";
+
+// Connect to the Mongo DB
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(console.log("DB CONNECTED: ", MONGODB_URI));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
